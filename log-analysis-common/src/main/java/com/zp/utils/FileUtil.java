2ecf9785859608b11d2d6c1e4ec07459a7cb0d2b
zhoupeng <287214764@qq.com>
2020-12-11 14:54:52 +0800

    新增按字节数读取文件方法

--- log-analysis-common/src/main/java/com/zp/utils/FileUtil.java
+++ log-analysis-common/src/main/java/com/zp/utils/FileUtil.java
@@ -3,6 +3,7 @@ package com.zp.utils;
 import io.netty.buffer.ByteBuf;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.nio.charset.Charset;
 
@@ -11,7 +12,7 @@ import java.nio.charset.Charset;
  * @create 2020/12/9 16:45
  */
 public class FileUtil {
-    public static void write(File file, String s){
+    public static void write(File file, String s) {
         try {
             FileOutputStream fileOutputStream = new FileOutputStream(file, true);
             fileOutputStream.write(s.getBytes(Charset.forName("UTF-8")));
@@ -20,7 +21,7 @@ public class FileUtil {
         }
     }
 
-    public static void write(File file, byte[] bytes){
+    public static void write(File file, byte[] bytes) {
         try {
             FileOutputStream fileOutputStream = new FileOutputStream(file, true);
             fileOutputStream.write(bytes);
@@ -29,7 +30,7 @@ public class FileUtil {
         }
     }
 
-    public static void write(File file, ByteBuf byteBuf){
+    public static void write(File file, ByteBuf byteBuf) {
         try {
             FileOutputStream fileOutputStream = new FileOutputStream(file, true);
             byte[] req = new byte[byteBuf.readableBytes()];
@@ -38,6 +39,19 @@ public class FileUtil {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static String read(File file, int bytes) {
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            byte[] buffer = new byte[bytes];
+            fileInputStream.read(buffer, 0, bytes);
+            return new String(buffer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+
     }
 
 }
